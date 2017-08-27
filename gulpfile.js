6ad3558dd0d7c1d6cf952672f9cd39c032e8cb83
grafiaviles <grafiaviles@gmail.com>
2017-08-26 21:09:27 -0300

    Entorno 90% listo, falta buscar una tarea para url php

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,49 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify'); //*
-const pump = require('pump'); //*
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const htmlmin = require('gulp-htmlmin');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'javascript', 'html'], function() {
 
     browserSync.init({
-        server: "./web-app"
+        server: "./www"
     });
 
-    gulp.watch("./web-app/js/*.js", ['comprimir']); //Para minificar JS
-    gulp.watch("./scss/**/*.scss", ['sass']); //Para SASS
-    gulp.watch("./web-app/*.html").on('change', browserSync.reload); //Para HTML
-    gulp.watch("./web-app/js/*.js").on('change', browserSync.reload); //Para JS
+    gulp.watch("./www/js/*.js", ['javascript']).on('change', browserSync.reload); //Observa la tarea minificar JS e inyecta
+    gulp.watch("./scss/**/*.scss", ['sass']); //Observa la tarea SASS
+    gulp.watch("./www/*.html").on('change', browserSync.reload); //Observa la tarea html y recarga
+    gulp.watch('./*.html', ['html']); //Observa la tarea minificar html
+});
+
+// MINIFICADOR HTML
+gulp.task('html', function() {
+    return gulp.src('./*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('www'));
 });
 
 // UGLIFY COMPRIMIR JS
-gulp.task('comprimir', function(cb) {
+gulp.task('javascript', function(cb) {
     pump([
-            gulp.src('./web-app/js/*.js'),
+            gulp.src('./www/js/*.js'),
             uglify(),
-            gulp.dest('./web-app/js/dist')
+            gulp.dest('./www/js/dist')
         ],
         cb
     );
 });
 
-// Sass Compile
+// SASS COMPILE
 gulp.task('sass', function() {
     return gulp.src('./scss/**/*.scss')
         .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(concat('main.min.css'))
-        .pipe(gulp.dest('./web-app/css'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('observar', function() {
-    gulp.watch('./scss/**/*.scss', ['sass'])
+        .pipe(gulp.dest('./www/css'))
+        .pipe(browserSync.stream())
 });
